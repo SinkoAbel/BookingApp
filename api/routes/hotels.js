8b81bbc7137a0cdd16b54d2bf54d640e4278bc3e
Sinkó Ábel <sinkoabel@gmail.com>
2022-10-13 19:30:29 +0200

    Refatored hotel api communication

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,62 +1,14 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import {createError} from "../utils/error.js";
+import {createHotel, deleteHotel, getAllHotels, getHotel, updateHotel} from "../controllers/HotelController.js";
 
 const router = express.Router();
 
-// CREATE
-router.post("/", async (req, res) => {
-    const newHotel = new Hotel(req.body)
-
-    try {
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// UPDATE
-router.put("/:id", async (req, res) => {
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(
-            req.params.id,
-            {$set: req.body},
-            {new: true}
-        );
-        res.status(200).json(updatedHotel);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// DELETE
-router.delete("/:id", async (req, res) => {
-    try {
-        await Hotel.findByIdAndDelete(req.params.id);
-        res.status(200).json("Hotel has been deleted.");
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// GET
-router.get("/:id", async (req, res) => {
-    try {
-        const hotel = await Hotel.findById(req.params.id);
-        res.status(200).json(hotel);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// GET ALL
-router.get("/", async (req, res) => {
-    try {
-        const getAllHotel = await Hotel.find();
-        res.status(200).json(getAllHotel);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+router.post("/", createHotel);
+router.put("/:id", updateHotel);
+router.delete("/:id", deleteHotel);
+router.get("/:id", getHotel);
+router.get("/", getAllHotels);
 
 export default router;
